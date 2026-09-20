a581b6bcbd46d9f73a9b5d9140d9803ab7866cfa
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-09-20 22:30:27 +0300

    Add project files.

---
+++ C#Basics-04.Answers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace C_Basics_04.Answers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+
+            String title = "Refactoring";
+
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice !");
+                    break;
+
+                case "Refractoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+
+                default:
+                    Console.WriteLine("Never heard of it"); 
+                    break;
+            }
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
